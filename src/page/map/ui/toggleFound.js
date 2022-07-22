91122994185adac3140dfac20172bfafcc9d34cb
MrFusiion <fuse-x-fuzionzz@outlook.be>
2022-07-22 18:51:38 +0200

    fixed togglefound

--- src/page/map/ui/toggleFound.js
+++ src/page/map/ui/toggleFound.js
@@ -4,7 +4,8 @@ class ToggleFound {
         this.$element = $(`<span id="toggle-found" class="button-toggle"><i class="icon ui-icon-show-hide"></i>Found Locations (0)</span>`)
             .insertAfter($(window.document).find("#toggle-found"))
             .click(() => {
-                window.mapManager.setFoundLocationsShown(!this.$element.toggleClass("disabled"));
+                this.$element.toggleClass("disabled");
+                window.mapManager.setFoundLocationsShown(!this.$element.hasClass("disabled"));
             });
     }
 
